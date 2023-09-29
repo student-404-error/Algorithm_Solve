0909738cb1cf82598b1a8dd92cfb8c68f924bf57
student-404-error <127807924+student-404-error@users.noreply.github.com>
2023-09-30 01:27:25 +0900

    CPP practice with g++, vim

---
+++ CPPAlgo/input.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int number = 0;
+	cin >> number;
+	cout << "your input is " << number << endl;
+
+	return 0;
+}
---
+++ CPPAlgo/vari.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int one = 1;
+	int two = 2;
+	int three = 3;
+
+	cout << "1 + 2 = " << one+two << endl;
+
+	return 0;
+}	
