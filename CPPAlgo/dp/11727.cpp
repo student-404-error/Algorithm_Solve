8c1ae8918fa4818953983b03ff87b3f92a6a8f99
student-404-error <127807924+student-404-error@users.noreply.github.com>
2024-06-16 18:31:01 +0200

    dp problem 3

---
+++ CPPAlgo/dp/11727.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main (int argc, char *argv[]) {
+	int	n;
+
+	cin >> n;
+	vector<int> dp(n, 0);
+	dp[0] = 1;
+	dp[1] = 3;
+	for (int i = 2; i < n; i++) {
+		dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007;
+	}
+	cout << dp[n - 1] << "\n";
+	return 0;
+}
